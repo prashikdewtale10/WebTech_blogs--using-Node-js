83aae3ab92adcb339ff09da3994ea6a3832fbe0f
Prashik Dewtale <prashikdewtale69@gmail.com>
2021-05-12 17:57:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express =require('express');
+const { Shape } = require('three');
+const bodyParser=require('body-parser')
+const {ServiceWorker} = require('node-service-worker');
+
+
+const app = express()
+const port = 5000
+
+// STatic files
+app.use(express.static('public'));
+app.use("/css",express.static(__dirname+"public/css"))
+app.use("/img",express.static(__dirname+"public/img"))
+app.use("/js",express.static(__dirname+"public/js"))
+
+// Templating engine
+app.set("views",'./src/views')
+app.set("view engine",'ejs')
+app.engine('ejs', require('ejs').__express);
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+// Routes 
+
+const newsRouter = require('./src/routes/news')
+app.use("/",newsRouter)
+app.use("/article",newsRouter)
+
+// app.get("/",(req,res)=>{
+//     res.write("Hello Prashik")
+// })
+
+// service worker
+const sw = new ServiceWorker({
+    scriptURL: 'https://localhost/sw.js',
+    scope: 'http://localhost', // Primarily to resolve relative URLs
+    contents: 'console.log("hi");' // The JS of the service worker
+})
+
+// app listening 
+app.listen(port,()=>{
+    console.log(`App is Listening on port ${port}`)
+})
---
+++ src/routes/news.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const newsRouter = express.Router()
+const axios = require('axios')
+
+
+
+newsRouter.get('',async(req,res)=>{
+     
+    try {
+
+      const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts`)
+      res.render('news',{articles:newsAPI.data}) 
+    } catch (err) {
+        if(err.response)
+        {
+            res.render('news',{articles:null}) 
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.header)
+        }else if(err.requiest){
+            res.render('news',{articles:null}) 
+            console.log(err.requiest)
+        }else{
+            res.render('news',{articles:null}) 
+            console.log('Error',err.message)
+        }
+        
+    }
+})
+
+newsRouter.get('/:id',async(req,res)=>{
+    let artID=req.params.id
+     
+    try {
+
+      const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts/${artID}`)
+      res.render('newsSingle',{article:newsAPI.data}) 
+    } catch (err) {
+        if(err.response)
+        {
+            res.render('newsSingle',{article:null}) 
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.header)
+        }else if(err.requiest){
+            res.render('newsSingle',{article:null}) 
+            console.log(err.requiest)
+        }else{
+            res.render('newsSingle',{article:null}) 
+            console.log('Error',err.message)
+        }
+        
+    }
+})
+
+newsRouter.post('',async(req,res)=>{
+    let search=req.body.search
+     
+    try {
+
+      const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts/search=${search}`)
+      res.render('newsSearch',{articles:newsAPI.data}) 
+    } catch (err) {
+        if(err.response)
+        {
+            <!-- res.render('newsSearch',{articles:null})  -->
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.header)
+        }else if(err.requiest){
+            res.render('newsSearch',{articles:null}) 
+            console.log(err.requiest)
+        }else{
+            res.render('newsSearch',{articles:null}) 
+            console.log('Error',err.message)
+        }
+        
+    }
+})
+
+module.exports= newsRouter
